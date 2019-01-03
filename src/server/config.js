1d1b1844aa8414c1a451daf447539a18bf4de810
moskdev <molchanovsk@gmail.com>
2019-01-03 16:09:41 +0200

    UIO-29/delete test port from config

--- src/server/config.js
+++ src/server/config.js
@@ -1,8 +1,7 @@
 const config = {};
 
 process.env.NODE_PORT = process.env.NODE_PORT || 3300;
-process.env.NODE_TEST_PORT = process.env.NODE_TEST_PORT || 3301;
 
-config.PORT = process.env.NODE_ENV === 'test' ? process.env.NODE_TEST_PORT : process.env.NODE_PORT;
+config.PORT = process.env.NODE_PORT;
 
 module.exports = config;
